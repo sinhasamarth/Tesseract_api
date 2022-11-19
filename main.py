50141a9f591f165b4abde076b8d93c7f460cef37
sinhasamarth1 <samarth.sinha@appyhigh.com>
2022-11-19 09:08:00 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,49 @@
+import pytesseract as pytesseract
+from PIL import Image
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return {"value": get_text()}
+
+
+def get_text():
+    pytesseract.pytesseract.tesseract_cmd = '/app/.apt/usr/bin/tesseract'
+    image = Image.open('./sam.png', mode='r')
+    raw = str(pytesseract.image_to_string(image))
+    raw.strip()
+    equals_pos = raw.rfind('=')
+    raw = raw[0:equals_pos].strip()
+    final = -1
+    if raw.__contains__('-'):
+        pos = raw.rfind('-')
+        first = int(raw[0:pos])
+        if raw[pos + 1: raw.__sizeof__()].__eq__("O0"):
+            second = 0
+        else:
+            second = int(raw[pos + 1: raw.__sizeof__()])
+        final = first - second
+
+    elif raw.__contains__('+'):
+        pos = raw.rfind('+')
+        first = int(raw[0:pos])
+        if raw[pos + 1: raw.__sizeof__()].__eq__("O0"):
+            second = 0
+        else:
+            second = int(raw[pos + 1: raw.__sizeof__()])
+        final = first + second
+
+    elif raw.__contains__('*'):
+        pos = raw.rfind('*')
+        first = int(raw[0:pos])
+        second = int(raw[pos + 1: raw.__sizeof__()])
+        final = first * second
+
+    return final
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=12345, debug=True)
