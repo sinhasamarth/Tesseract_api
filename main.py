16ce9bc8e30ac9c944c1c921f285735748e81a57
sinhasamarth1 <samarthkumar192@gmail.com>
2022-11-19 12:55:51 +0530

    Fixed Bugs

--- main.py
+++ main.py
@@ -12,36 +12,36 @@ def hello():
     return {"a": 'a'}
 
 
-def get_text():
-    pytesseract.pytesseract.tesseract_cmd = './.apt/usr/share/tesseract-ocr/4.00/tessdata'
-    image = Image.open('./sam.png', mode='r')
-    raw = str(pytesseract.image_to_string(image))
-    raw.strip()
-    equals_pos = raw.rfind('=')
-    raw = raw[0:equals_pos].strip()
-    final = -1
-    if raw.__contains__('-'):
-        pos = raw.rfind('-')
-        first = int(raw[0:pos])
-        if raw[pos + 1: raw.__sizeof__()].__eq__("O0"):
-            second = 0
-        else:
-            second = int(raw[pos + 1: raw.__sizeof__()])
-        final = first - second
-
-    elif raw.__contains__('+'):
-        pos = raw.rfind('+')
-        first = int(raw[0:pos])
-        if raw[pos + 1: raw.__sizeof__()].__eq__("O0"):
-            second = 0
-        else:
-            second = int(raw[pos + 1: raw.__sizeof__()])
-        final = first + second
-
-    elif raw.__contains__('*'):
-        pos = raw.rfind('*')
-        first = int(raw[0:pos])
-        second = int(raw[pos + 1: raw.__sizeof__()])
-        final = first * second
-
-    return final
+# def get_text():
+#     pytesseract.pytesseract.tesseract_cmd = './.apt/usr/share/tesseract-ocr/4.00/tessdata'
+#     image = Image.open('./sam.png', mode='r')
+#     raw = str(pytesseract.image_to_string(image))
+#     raw.strip()
+#     equals_pos = raw.rfind('=')
+#     raw = raw[0:equals_pos].strip()
+#     final = -1
+#     if raw.__contains__('-'):
+#         pos = raw.rfind('-')
+#         first = int(raw[0:pos])
+#         if raw[pos + 1: raw.__sizeof__()].__eq__("O0"):
+#             second = 0
+#         else:
+#             second = int(raw[pos + 1: raw.__sizeof__()])
+#         final = first - second
+#
+#     elif raw.__contains__('+'):
+#         pos = raw.rfind('+')
+#         first = int(raw[0:pos])
+#         if raw[pos + 1: raw.__sizeof__()].__eq__("O0"):
+#             second = 0
+#         else:
+#             second = int(raw[pos + 1: raw.__sizeof__()])
+#         final = first + second
+#
+#     elif raw.__contains__('*'):
+#         pos = raw.rfind('*')
+#         first = int(raw[0:pos])
+#         second = int(raw[pos + 1: raw.__sizeof__()])
+#         final = first * second
+#
+#     return final
